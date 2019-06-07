99e69312201b1b90aee7896f5aa3151794039f8d
Eric <github@esellors.com>
2019-06-07 11:46:27 -0500

    part 1 step 2 complete

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import routes from './routes';
 import './App.css';
 import Header from './Components/Header/Header';
-import Dashboard from './Components/Dashboard/Dashboard';
-import Wizard from './Components/Wizard/Wizard';
 
 function App() {
   return (
--- src/Components/Wizard/Wizard.js
+++ src/Components/Wizard/Wizard.js
@@ -2,11 +2,91 @@ import React, {Component} from 'react';
 import {Link} from 'react-router-dom';
 
 class Wizard extends Component {
+   constructor(props) {
+      super(props);
+      this.state = {
+         name: '',
+         address: '',
+         city: '',
+         state: '',
+         zip: 0
+      }
+      this.handleInputChange = this.handleInputChange.bind(this);
+   }
+   handleInputChange(e) {
+      console.log('hit')
+      const inputID = e.target.id;
+      const inputValue = e.target.value;
+      console.log(inputID)
+
+      switch(inputID) {
+         case 'input_name':
+            this.setState({
+               name: inputValue
+            });
+            break;
+         case 'input_address':
+            this.setState({
+               address: inputValue
+            });
+            break;
+         case 'input_city':
+            this.setState({
+               city: inputValue
+            });
+            break;
+         case 'input_state':
+            this.setState({
+               state: inputValue
+            });
+            break;
+         case 'input_zip':
+            this.setState({
+               zip: inputValue
+            });
+            break;
+         default:
+            break;
+      }
+   }
    render() {
       return (
          <div id='wizard_container'>
-            <h1>Wizard</h1>
-            <Link to='/'><button>Cancel</button></Link>
+            <header>
+               <h1>Wizard</h1>
+               <Link to='/'><button>Cancel</button></Link>
+            </header>
+            <main>
+               <form>
+                  <label htmlFor='input_name'>
+                     Property Name
+                  </label>
+                  <input id='input_name' type='text' value={this.state.name} onChange={this.handleInputChange} />
+
+                  <label htmlFor='input_address'>
+                     Address
+                  </label>
+                  <input id='input_address' type='text' value={this.state.address} onChange={this.handleInputChange} />
+
+                  <label htmlFor='input_city'>
+                     City
+                  </label>
+                  <input id='input_city' type='text' value={this.state.city} onChange={this.handleInputChange} />
+
+                  <span id='state_zip_container'>
+                     <label htmlFor='input_state'>
+                        State
+                     </label>
+                     <input id='input_state' type='text' value={this.state.state} onChange={this.handleInputChange} />
+
+                     <label htmlFor='input_zip'>
+                        ZIP
+                     </label>
+                     <input id='input_zip' type='text' value={this.state.zip} onChange={this.handleInputChange} />
+                  </span>
+               </form>
+            </main>
+
          </div>
       );
    }
