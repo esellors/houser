d04bba106daa9e9d45f744389ed6ee472445412b
Eric <github@esellors.com>
2019-06-07 10:45:32 -0500

    frontend, backend, database structures set up

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 require('dotenv').config();
 const express = require('express');
+const massive = require('massive');
 const app = express();
-const {SERVER_PORT} = process.env;
+const {SERVER_PORT, DATABASE_STRING} = process.env;
 
 app.use(express.json());
 
-
+massive(DATABASE_STRING).then(db => {
+   app.set('db', db);
+   console.log('Dabase connected');
+});
 
 
 
